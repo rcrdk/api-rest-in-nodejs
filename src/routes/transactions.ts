5e5de7e681998141aa7531819125811c815596da
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-08-28 10:05:36 -0300

    test: add e2e tests to transactions routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -45,7 +45,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
         .sum('amount', { as: 'amount' })
         .first()
 
-      return summary
+      return { summary }
     },
   )
 
