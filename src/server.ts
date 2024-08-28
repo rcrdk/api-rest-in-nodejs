7dd22bc96d480dc09dff1e66d9362edb82068aa5
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2024-08-28 10:42:49 -0300

    ci: fix connection settings for production

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,6 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => console.log('HTTP Server Running.'))
